ebbf0272128fc8132b1bd52a274d05d691997ff7
navneet199 <105769385+navneet199@users.noreply.github.com>
2023-01-04 15:42:58 +0530

    Update mongoose.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -3,7 +3,7 @@ mongoose.connect('mongodb://localhost/super_market');  //connect db
 const db = mongoose.connection;  //acquire the connection
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
-  console.log('Success db');
+  console.log('Success db connection');
 });
 
 
